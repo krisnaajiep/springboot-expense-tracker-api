673c5181d6c0fb18aef1c2bb44c58086d6186aca
krisnaajiep <krisnaajiep@gmail.com>
2025-05-14 09:28:17 +0700

    first commit

---
+++ src/main/java/com/krisnaajiep/expensetrackerapi/ExpenseTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.krisnaajiep.expensetrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krisnaajiep/expensetrackerapi/ExpenseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krisnaajiep.expensetrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
