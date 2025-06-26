5893d72e493bca94f4b115bd6e585d0a1ec9b7ee
krisnaajiep <krisnaajiep@gmail.com>
2025-06-27 02:53:01 +0700

    feat(user): add UserRepository interface with existsByEmail method

---
+++ src/main/java/com/krisnaajiep/expensetrackerapi/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.krisnaajiep.expensetrackerapi.repository;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 27/06/25 02.50
+@Last Modified 27/06/25 02.50
+Version 1.0
+*/
+
+import com.krisnaajiep.expensetrackerapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+
+    // Additional query methods can be defined here if needed
+}
