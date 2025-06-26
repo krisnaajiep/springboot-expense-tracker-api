ffa6a168f8d5d0001ea410d7282fe08d31916c6c
krisnaajiep <krisnaajiep@gmail.com>
2025-06-27 02:20:24 +0700

    feat(user): add Auditable and User entity classes with auditing fields

---
+++ src/main/java/com/krisnaajiep/expensetrackerapi/model/Auditable.java
@@ -0,0 +1,35 @@
+package com.krisnaajiep.expensetrackerapi.model;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 14/05/25 10.37
+@Last Modified 14/05/25 10.37
+Version 1.0
+*/
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+    @CreatedDate
+    @Column(name = "CreatedAt", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "UpdatedAt", nullable = false, insertable = false)
+    private LocalDateTime updatedAt;
+}
