3daebbb27bb7275d5e00179f46bcdccee4527763
krisnaajiep <krisnaajiep@gmail.com>
2025-06-27 23:17:53 +0700

    feat(api): add GlobalExceptionHandler to centralize exception handling for validation errors

---
+++ src/main/java/com/krisnaajiep/expensetrackerapi/handler/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.krisnaajiep.expensetrackerapi.handler;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 27/06/25 22.57
+@Last Modified 27/06/25 22.57
+Version 1.0
+*/
+
+import lombok.NonNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request
+    ) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return new ResponseEntity<>(Map.of("errors", errors), headers, status);
+    }
+}
