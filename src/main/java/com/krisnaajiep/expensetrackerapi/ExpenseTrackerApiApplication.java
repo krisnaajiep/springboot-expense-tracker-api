cbb4a8de70fb36c420e65599172395c0adc3ffa2
krisnaajiep <krisnaajiep@gmail.com>
2025-06-28 03:11:18 +0700

    feat(api): enable JPA auditing in the application configuration

--- src/main/java/com/krisnaajiep/expensetrackerapi/ExpenseTrackerApiApplication.java
+++ src/main/java/com/krisnaajiep/expensetrackerapi/ExpenseTrackerApiApplication.java
@@ -2,8 +2,10 @@ package com.krisnaajiep.expensetrackerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ExpenseTrackerApiApplication {
 
     public static void main(String[] args) {
