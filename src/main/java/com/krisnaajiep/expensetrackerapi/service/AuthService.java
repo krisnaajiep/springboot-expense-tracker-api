540db9def691fdb2a3ecb781242cf4b09d31f84c
krisnaajiep <krisnaajiep@gmail.com>
2025-06-28 07:31:55 +0700

    feat(auth): implement register method in AuthService with token generation
    - Add user registration functionality, including password encoding and conflict checks.
    - Integrate JWT generation for access token creation.

--- src/main/java/com/krisnaajiep/expensetrackerapi/service/AuthService.java
+++ src/main/java/com/krisnaajiep/expensetrackerapi/service/AuthService.java
@@ -10,12 +10,37 @@ Created on 27/06/25 02.53
 Version 1.0
 */
 
+import com.krisnaajiep.expensetrackerapi.dto.request.RegisterRequestDto;
+import com.krisnaajiep.expensetrackerapi.dto.response.TokenResponseDto;
+import com.krisnaajiep.expensetrackerapi.handler.exception.ConflictException;
+import com.krisnaajiep.expensetrackerapi.mapper.UserMapper;
+import com.krisnaajiep.expensetrackerapi.model.User;
 import com.krisnaajiep.expensetrackerapi.repository.UserRepository;
+import com.krisnaajiep.expensetrackerapi.security.JwtUtility;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AuthService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtility jwtUtility;
+
+    public TokenResponseDto register(RegisterRequestDto registerRequestDto) {
+        if (userRepository.existsByEmail(registerRequestDto.getEmail())) {
+            throw new ConflictException("User with this email already exists");
+        }
+
+        User user = UserMapper.toUser(registerRequestDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        User savedUser = userRepository.save(user);
+
+        String accessToken = jwtUtility.generateToken(savedUser.getId().toString(), savedUser.getEmail());
+
+        return new TokenResponseDto(accessToken);
+    }
 }
