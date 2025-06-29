efdbbc6e74ea69f208077643adb4d0b7548f3430
krisnaajiep <krisnaajiep@gmail.com>
2025-06-29 08:38:07 +0700

    feat(security): implement UserDetailsService with custom user loading by email
    - Add `UserDetailsServiceImpl` to handle user retrieval by email using `UserRepository`.
    - Utilize `CustomUserDetails` for integration with Spring Security.

---
+++ src/main/java/com/krisnaajiep/expensetrackerapi/security/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.krisnaajiep.expensetrackerapi.security;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 28/06/25 23.55
+@Last Modified 28/06/25 23.55
+Version 1.0
+*/
+
+import com.krisnaajiep.expensetrackerapi.model.CustomUserDetails;
+import com.krisnaajiep.expensetrackerapi.model.User;
+import com.krisnaajiep.expensetrackerapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsService")
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+
+        return new CustomUserDetails(user);
+    }
+}
