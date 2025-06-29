18bca2a50ffa294bb3d83ece4ca75c4247f67118
krisnaajiep <krisnaajiep@gmail.com>
2025-06-29 08:33:18 +0700

    feat(user): add CustomUserDetails implementation for Spring Security
    - Provide a custom implementation of UserDetails to integrate with Spring Security.

---
+++ src/main/java/com/krisnaajiep/expensetrackerapi/model/CustomUserDetails.java
@@ -0,0 +1,42 @@
+package com.krisnaajiep.expensetrackerapi.model;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 29/06/25 00.02
+@Last Modified 29/06/25 00.02
+Version 1.0
+*/
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+}
