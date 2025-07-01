c65c1181d9a6987a16a1f9fdbb300b798dd745d9
krisnaajiep <krisnaajiep@gmail.com>
2025-07-02 04:19:29 +0700

    test(expense): add unit tests for update functionality in ExpenseService
    - Cover success and failure scenarios: not found and access denied cases.
    - Mock dependencies for repository interactions and validations.

--- src/test/java/com/krisnaajiep/expensetrackerapi/service/ExpenseServiceTest.java
+++ src/test/java/com/krisnaajiep/expensetrackerapi/service/ExpenseServiceTest.java
@@ -1,9 +1,11 @@
 package com.krisnaajiep.expensetrackerapi.service;
 
 import com.krisnaajiep.expensetrackerapi.dto.response.ExpenseResponseDto;
+import com.krisnaajiep.expensetrackerapi.handler.exception.NotFoundException;
 import com.krisnaajiep.expensetrackerapi.model.Expense;
 import com.krisnaajiep.expensetrackerapi.model.User;
 import com.krisnaajiep.expensetrackerapi.repository.ExpenseRepository;
+import com.krisnaajiep.expensetrackerapi.util.SecureRandomUtility;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,9 +13,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.access.AccessDeniedException;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -33,7 +37,7 @@ class ExpenseServiceTest {
     void setUp() {
         user.setId(1L);
         user.setEmail("john@doe");
-        user.setPassword("password123");
+        user.setPassword(SecureRandomUtility.generateRandomString(8));
         user.setName("John Doe");
 
         expense.setId(1L);
@@ -62,5 +66,60 @@ class ExpenseServiceTest {
         assertEquals(expense.getDate(), response.getDate());
 
         verify(expenseRepository, times(1)).save(expense);
+    }
+
+    @Test
+    void testUpdateFailure_ExpenseNotFound() {
+        when(expenseRepository.findById(expense.getId())).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> expenseService.update(expense.getId(), expense));
+
+        verify(expenseRepository, times(1)).findById(expense.getId());
+        verifyNoMoreInteractions(expenseRepository);
+    }
+
+    @Test
+    void testUpdateFailure_AccessDenied() {
+        User anotherUser = User.builder()
+                .id(2L)
+                .email("jane@doe")
+                .password(SecureRandomUtility.generateRandomString(8))
+                .name("Jane Doe")
+                .build();
+
+        Expense anotherExpense = Expense.builder()
+                .id(2L)
+                .description("Another Expense")
+                .amount(new BigDecimal("200.00"))
+                .category(Expense.Category.fromDisplayName("Others"))
+                .date(LocalDate.now())
+                .user(anotherUser)
+                .build();
+
+        System.out.printf("Another Expense User ID: %d%n", anotherExpense.getUser().getId());
+        System.out.printf("Original Expense User ID: %d%n", expense.getUser().getId());
+
+        when(expenseRepository.findById(expense.getId())).thenReturn(Optional.of(expense));
+
+        assertThrows(AccessDeniedException.class, () -> expenseService.update(expense.getId(), anotherExpense));
+
+        verify(expenseRepository, times(1)).findById(expense.getId());
+        verifyNoMoreInteractions(expenseRepository);
+    }
+
+    @Test
+    void testUpdateSuccess() {
+        when(expenseRepository.findById(expense.getId())).thenReturn(Optional.of(expense));
+
+        ExpenseResponseDto response = expenseService.update(expense.getId(), expense);
+
+        assertNotNull(response);
+        assertEquals(expense.getId(), response.getId());
+        assertEquals(expense.getDescription(), response.getDescription());
+        assertEquals(expense.getAmount(), response.getAmount());
+        assertEquals(expense.getCategory().getDisplayName(), response.getCategory());
+        assertEquals(expense.getDate(), response.getDate());
+
+        verify(expenseRepository, times(1)).findById(expense.getId());
     }
 }
