cc37fec07e9c214d4aa1c8018f52f21726b64cb1
krisnaajiep <krisnaajiep@gmail.com>
2025-07-01 12:38:03 +0700

    feat(expense): add Expense entity with category enumeration
    - Introduce `Expense` entity class with fields: id, user, description, amount, category, and date.
    - Add `Category` enum to categorize expenses with display name support.
    - Annotate fields for JPA/Hibernate integration with validation constraints.

---
+++ src/main/java/com/krisnaajiep/expensetrackerapi/model/Expense.java
@@ -0,0 +1,72 @@
+package com.krisnaajiep.expensetrackerapi.model;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 30/06/25 01.46
+@Last Modified 30/06/25 01.46
+Version 1.0
+*/
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class Expense extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "UserID", nullable = false)
+    private User user;
+
+    @Column(name = "Description", nullable = false)
+    private String description;
+
+    @Column(name = "Amount", nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Category", nullable = false, length = 20)
+    private Category category;
+
+    @Column(name = "Date", nullable = false)
+    private LocalDate date;
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Category {
+        GROCERIES ("Groceries"),
+        LEISURE ("Leisure"),
+        ELECTRONICS ("Electronics"),
+        UTILITIES ("Utilities"),
+        CLOTHING ("Clothing"),
+        HEALTH ("Health"),
+        OTHERS ("Others");
+
+        private final String displayName;
+
+        public static Category fromDisplayName(String displayName) {
+            for (Category category : Category.values()) {
+                if (category.getDisplayName().equals(displayName)) {
+                    return category;
+                }
+            }
+
+            throw new IllegalArgumentException("No category found for display name: " + displayName);
+        }
+    }
+}
+
