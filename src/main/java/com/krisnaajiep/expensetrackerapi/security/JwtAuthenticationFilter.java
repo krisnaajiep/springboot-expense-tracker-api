35ff70afeddc544b77f2b440eb185e72113dcf22
krisnaajiep <krisnaajiep@gmail.com>
2025-07-01 13:07:38 +0700

    feat(security): add JwtAuthenticationFilter for processing JWT in request filtering
    - Introduce `JwtAuthenticationFilter` extending `OncePerRequestFilter` to handle JWT extraction and authentication.
    - Parse tokens from the `Authorization` header, authenticate via `AuthenticationManager`, and set the security context.

---
+++ src/main/java/com/krisnaajiep/expensetrackerapi/security/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.krisnaajiep.expensetrackerapi.security;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 27/06/25 19.01
+@Last Modified 27/06/25 19.01
+Version 1.0
+*/
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final Logger log = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        String token = extractTokenFromHeader(request);
+
+        try {
+            if (token != null) {
+                JwtAuthenticationToken jwtAuthenticationToken = new JwtAuthenticationToken(token);
+                jwtAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                Authentication authentication = authenticationManager.authenticate(jwtAuthenticationToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            log.error("Authentication filter error", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractTokenFromHeader(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        return authorizationHeader != null && authorizationHeader.startsWith("Bearer ")
+                ? authorizationHeader.substring(7)
+                : null;
+    }
+}
