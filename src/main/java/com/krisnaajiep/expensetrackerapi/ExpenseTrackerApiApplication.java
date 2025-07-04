276c57a95553af426dfe34e8f7281bd1adc1a5b4
krisnaajiep <krisnaajiep@gmail.com>
2025-07-04 08:27:57 +0700

    feat(env): add EnvUtility for setting environment variables
    - Introduce `EnvUtility` to load and set environment variables using `dotenv` library.
    - Update `ExpenseTrackerApiApplication` to initialize environment variables at startup.

--- src/main/java/com/krisnaajiep/expensetrackerapi/ExpenseTrackerApiApplication.java
+++ src/main/java/com/krisnaajiep/expensetrackerapi/ExpenseTrackerApiApplication.java
@@ -1,5 +1,6 @@
 package com.krisnaajiep.expensetrackerapi;
 
+import com.krisnaajiep.expensetrackerapi.util.EnvUtility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class ExpenseTrackerApiApplication {
 
     public static void main(String[] args) {
+        // Set env
+        EnvUtility.setEnv();
+
         SpringApplication.run(ExpenseTrackerApiApplication.class, args);
     }
 
