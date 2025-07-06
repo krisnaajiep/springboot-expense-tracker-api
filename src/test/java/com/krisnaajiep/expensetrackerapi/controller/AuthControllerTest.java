1f90edcf70b1488999df960315126a5bd6682e9c
krisnaajiep <krisnaajiep@gmail.com>
2025-07-06 15:46:25 +0700

    test(auth): extend AuthController tests with refresh token scenarios
    - Add test coverage for refresh token use cases including success, bad request, not found, and expired token scenarios.
    - Enhance test setup to include `RefreshTokenRepository` for proper entity management.
    - Verify response structure and field values in all test cases.

--- src/test/java/com/krisnaajiep/expensetrackerapi/controller/AuthControllerTest.java
+++ src/test/java/com/krisnaajiep/expensetrackerapi/controller/AuthControllerTest.java
@@ -3,13 +3,17 @@ package com.krisnaajiep.expensetrackerapi.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.krisnaajiep.expensetrackerapi.dto.request.LoginRequestDto;
+import com.krisnaajiep.expensetrackerapi.dto.request.RefreshTokenRequestDto;
 import com.krisnaajiep.expensetrackerapi.dto.request.RegisterRequestDto;
 import com.krisnaajiep.expensetrackerapi.dto.response.TokenResponseDto;
+import com.krisnaajiep.expensetrackerapi.model.RefreshToken;
 import com.krisnaajiep.expensetrackerapi.model.User;
 import com.krisnaajiep.expensetrackerapi.repository.ExpenseRepository;
+import com.krisnaajiep.expensetrackerapi.repository.RefreshTokenRepository;
 import com.krisnaajiep.expensetrackerapi.repository.UserRepository;
 import com.krisnaajiep.expensetrackerapi.security.JwtUtility;
 import com.krisnaajiep.expensetrackerapi.util.SecureRandomUtility;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,6 +25,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.Instant;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,6 +38,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AuthControllerTest {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
 
     @Autowired
     private ExpenseRepository expenseRepository;
@@ -51,6 +59,7 @@ class AuthControllerTest {
 
     private final RegisterRequestDto registerRequestDto = new RegisterRequestDto();
     private final LoginRequestDto loginRequestDto = new LoginRequestDto();
+    private final RefreshTokenRequestDto refreshTokenRequestDto = new RefreshTokenRequestDto();
 
     private static final String USER_NAME = "John Doe";
     private static final String USER_EMAIL = "john@doe.com";
@@ -60,6 +69,7 @@ class AuthControllerTest {
     void setUp() {
         // Clean up the database before each test
         expenseRepository.deleteAll();
+        refreshTokenRepository.deleteAll();
         userRepository.deleteAll();
 
         passwordEncoder = new BCryptPasswordEncoder();
@@ -166,6 +176,10 @@ class AuthControllerTest {
             System.out.printf("Email from token: %s%n", email);
 
             assertEquals(registerRequestDto.getEmail(), email);
+
+            assertNotNull(response.getRefreshToken());
+
+            System.out.printf("Refresh token: %s%n", response.getRefreshToken());
         });
     }
 
@@ -264,6 +278,143 @@ class AuthControllerTest {
             System.out.printf("Email from token: %s%n", email);
 
             assertEquals(loginRequestDto.getEmail(), email);
+
+            assertNotNull(response.getRefreshToken());
+
+            System.out.printf("Refresh token: %s%n", response.getRefreshToken());
         });
+    }
+
+    @Test
+    public void testRefreshBadRequest() throws Exception {
+        refreshTokenRequestDto.setRefreshToken(null);
+
+        mockMvc.perform(post("/refresh")
+                .accept(MediaType.ALL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(refreshTokenRequestDto))
+        ).andExpect(
+                status().isBadRequest()
+        ).andDo(result -> {
+            Map<String, Object> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<>() {
+                    }
+            );
+
+            System.out.printf("response: %s%n", response);
+
+            assertNotNull(response);
+            assertNotNull(response.get("errors"));
+            assertEquals(1, ((Map<?, ?>) response.get("errors")).size());
+            assertEquals("must not be blank", ((Map<?, ?>) response.get("errors")).get("refresh-token"));
+        });
+    }
+
+    @Test
+    public void testRefreshNotFound() throws Exception {
+        refreshTokenRequestDto.setRefreshToken("invalid refresh token");
+
+        mockMvc.perform(post("/refresh")
+                .accept(MediaType.ALL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(refreshTokenRequestDto))
+        ).andExpect(
+                status().isUnauthorized()
+        ).andDo(result -> {
+            Map<String, Object> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<>() {
+                    }
+            );
+
+            System.out.printf("response: %s%n", response);
+
+            assertNotNull(response);
+            assertNotNull(response.get("message"));
+            assertEquals("Invalid refresh token", response.get("message"));
+        });
+    }
+
+    @Test
+    public void testRefreshExpired() throws Exception {
+        String rawRefreshToken = SecureRandomUtility.generateRandomString(32);
+        setRefreshToken(rawRefreshToken, Instant.now().minusSeconds(3600));
+        refreshTokenRequestDto.setRefreshToken(rawRefreshToken);
+
+        mockMvc.perform(post("/refresh")
+                .accept(MediaType.ALL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(refreshTokenRequestDto))
+        ).andExpect(
+                status().isUnauthorized()
+        ).andDo(result -> {
+            Map<String, Object> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<>() {
+                    }
+            );
+
+            System.out.printf("response: %s%n", response);
+
+            assertNotNull(response);
+            assertNotNull(response.get("message"));
+            assertEquals("Refresh token expired", response.get("message"));
+        });
+    }
+
+    @Test
+    public void testRefreshSuccess() throws Exception {
+        String rawRefreshToken = SecureRandomUtility.generateRandomString(32);
+        setRefreshToken(rawRefreshToken, Instant.now().plusMillis(86400000));
+        refreshTokenRequestDto.setRefreshToken(rawRefreshToken);
+
+        mockMvc.perform(post("/refresh")
+                .accept(MediaType.ALL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(refreshTokenRequestDto))
+        ).andExpect(
+                status().isOk()
+        ).andDo(result -> {
+            TokenResponseDto response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<>() {}
+            );
+
+            System.out.printf("response: %s%n", response);
+
+            assertNotNull(response);
+
+            assertNotNull(response.getAccessToken());
+
+            String accessToken = response.getAccessToken();
+
+            assertNotNull(jwtUtility.getEmail(accessToken));
+
+            String email = jwtUtility.getEmail(accessToken);
+
+            System.out.printf("Email from token: %s%n", email);
+
+            assertNotNull(response.getRefreshToken());
+
+            System.out.printf("New refresh token: %s%n", response.getRefreshToken());
+        });
+    }
+
+    private void setRefreshToken(String token, Instant expiryDate) {
+        User user = User.builder()
+                .name(USER_NAME)
+                .email(USER_EMAIL)
+                .password(passwordEncoder.encode(USER_PASSWORD))
+                .build();
+
+        RefreshToken refreshToken = RefreshToken.builder()
+                .token(DigestUtils.sha256Hex(token))
+                .expiryDate(expiryDate)
+                .user(user)
+                .build();
+
+        userRepository.save(user);
+        refreshTokenRepository.save(refreshToken);
     }
 }
