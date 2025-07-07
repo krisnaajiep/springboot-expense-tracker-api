cbcfbe68cdac310427a82580f2999f0b37608d55
krisnaajiep <krisnaajiep@gmail.com>
2025-07-07 13:09:39 +0700

    feat(config): add FilterConfig to register RateLimitFilter
    - Introduce `FilterConfig` class for managing servlet filter registrations.
    - Register `RateLimitFilter` with a defined order.

---
+++ src/main/java/com/krisnaajiep/expensetrackerapi/config/FilterConfig.java
@@ -0,0 +1,26 @@
+package com.krisnaajiep.expensetrackerapi.config;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 07/07/25 08.50
+@Last Modified 07/07/25 08.50
+Version 1.0
+*/
+
+import com.krisnaajiep.expensetrackerapi.filter.RateLimitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<RateLimitFilter> registerRateLimitFilter(RateLimitFilter filter) {
+        FilterRegistrationBean<RateLimitFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setOrder(1);
+        return registration;
+    }
+}
