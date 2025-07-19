7faa96f99eee9dae5c0da9b6bdae1f5df5df034c
krisnaajiep <krisnaajiep@gmail.com>
2025-07-19 21:07:01 +0700

    refactor: standardize imports across DTOs, controllers, and models for consistency
    - Organized imports in DTOs, controllers, and model classes.
    - Replaced wildcard imports with explicit ones to enhance code clarity.

--- src/main/java/com/krisnaajiep/expensetrackerapi/model/Expense.java
+++ src/main/java/com/krisnaajiep/expensetrackerapi/model/Expense.java
@@ -10,8 +10,23 @@ Created on 30/06/25 01.46
 Version 1.0
 */
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
--- src/main/java/com/krisnaajiep/expensetrackerapi/model/User.java
+++ src/main/java/com/krisnaajiep/expensetrackerapi/model/User.java
@@ -10,8 +10,17 @@ Created on 14/05/25 10.21
 Version 1.0
 */
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 
 @Data
