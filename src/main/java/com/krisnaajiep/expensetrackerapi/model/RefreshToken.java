8839fc088a526ce1c3e70fd23ba741f885181fe2
krisnaajiep <krisnaajiep@gmail.com>
2025-07-19 21:07:01 +0700

    refactor: standardize imports across DTOs, controllers, and models for consistency
    - Organized imports in DTOs, controllers, and model classes.
    - Replaced wildcard imports with explicit ones to enhance code clarity.

--- src/main/java/com/krisnaajiep/expensetrackerapi/model/RefreshToken.java
+++ src/main/java/com/krisnaajiep/expensetrackerapi/model/RefreshToken.java
@@ -10,8 +10,19 @@ Created on 05/07/25 11.11
 Version 1.0
 */
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
