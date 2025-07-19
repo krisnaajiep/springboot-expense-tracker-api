65877e4edb234c01fad4fa2f54f4e2a3005c1535
krisnaajiep <krisnaajiep@gmail.com>
2025-07-19 18:39:17 +0700

    test: set active profile to "test" for integration tests
    - Add `@ActiveProfiles("test")` to integration test classes for consistent test environment configuration.

--- src/test/java/com/krisnaajiep/expensetrackerapi/ExpenseTrackerApiApplicationTests.java
+++ src/test/java/com/krisnaajiep/expensetrackerapi/ExpenseTrackerApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.krisnaajiep.expensetrackerapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExpenseTrackerApiApplicationTests {
 
     @Test
