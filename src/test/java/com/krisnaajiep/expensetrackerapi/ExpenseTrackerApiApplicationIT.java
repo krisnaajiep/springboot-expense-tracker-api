6df81428b19d9f3c0afd0ea9dd903583a5b255d9
krisnaajiep <krisnaajiep@gmail.com>
2025-07-21 00:28:02 +0700

    test: rename test classes to follow integration test naming convention
    - Renamed `*Test` classes to `*IT` to align with integration test naming conventions.

--- src/test/java/com/krisnaajiep/expensetrackerapi/ExpenseTrackerApiApplicationIT.java
+++ src/test/java/com/krisnaajiep/expensetrackerapi/ExpenseTrackerApiApplicationIT.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ExpenseTrackerApiApplicationTests {
+class ExpenseTrackerApiApplicationIT {
 
     @Test
     void contextLoads() {
--- src/test/java/com/krisnaajiep/expensetrackerapi/controller/ExpenseControllerIT.java
+++ src/test/java/com/krisnaajiep/expensetrackerapi/controller/ExpenseControllerIT.java
@@ -36,7 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-class ExpenseControllerTest {
+class ExpenseControllerIT {
     @Autowired
     private ExpenseRepository expenseRepository;
 
