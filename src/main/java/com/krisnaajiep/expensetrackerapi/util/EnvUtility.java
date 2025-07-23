a18513c856e2d99ce453047ba6305b65dbe322b9
krisnaajiep <krisnaajiep@gmail.com>
2025-07-23 19:44:09 +0700

    refactor: migrate datasource environment variable names to align with Spring Boot conventions
    - Replaced `DB_` prefixed environment variables with `SPRING_DATASOURCE_` for consistency.
    - Updated `application.properties` and `.env.example` to reflect the new variable names.
    - Adjusted `EnvUtility` to handle updated environment variables.
    - Set Hikari connection pool size to 5 in application properties

--- src/main/java/com/krisnaajiep/expensetrackerapi/util/EnvUtility.java
+++ src/main/java/com/krisnaajiep/expensetrackerapi/util/EnvUtility.java
@@ -18,7 +18,11 @@ import java.util.stream.Stream;
 public class EnvUtility {
     public static void setEnv() {
         Dotenv dotenv = Dotenv.load();
-        Stream.of("DB_HOST", "DB_PORT", "DB_NAME", "DB_USER", "DB_PASS", "JWT_SECRET")
-                .forEach(envVar -> System.setProperty(envVar, Objects.requireNonNull(dotenv.get(envVar))));
+        Stream.of(
+                "SPRING_DATASOURCE_URL",
+                        "SPRING_DATASOURCE_USERNAME",
+                        "SPRING_DATASOURCE_PASSWORD",
+                        "JWT_SECRET"
+                ).forEach(envVar -> System.setProperty(envVar, Objects.requireNonNull(dotenv.get(envVar))));
     }
 }
