e3e0a7f4bdec3d6a98129ebae7a7612e57a7dd11
krisnaajiep <krisnaajiep@gmail.com>
2025-08-06 23:24:02 +0700

    docs: update package-info documentation with class references
    - Revised package-info files across multiple packages to include detailed class references, enhancing navigation and understanding of package contents.

--- src/main/java/com/krisnaajiep/expensetrackerapi/dto/response/package-info.java
+++ src/main/java/com/krisnaajiep/expensetrackerapi/dto/response/package-info.java
@@ -2,10 +2,9 @@
  * This package contains Data Transfer Object (DTO) classes used for API responses.
  * It includes:
  * <ul>
- *     <li>Response objects for REST endpoints</li>
- *     <li>Standardized response formats</li>
- *     <li>Data structures for client-side consumption</li>
- *     <li>Response wrappers and result containers</li>
+ *     <li>{@link com.krisnaajiep.expensetrackerapi.dto.response.TokenResponseDto} representing the response body for access dan refresh tokens.</li>
+ *     <li>{@link com.krisnaajiep.expensetrackerapi.dto.response.ExpenseResponseDto} representing the response body for an expense.</li>
+ *     <li>{@link com.krisnaajiep.expensetrackerapi.dto.response.PagedResponseDto} representing a paged response structure.</li>
  * </ul>
  *
  * @since 0.0.1
--- src/main/java/com/krisnaajiep/expensetrackerapi/security/config/package-info.java
+++ src/main/java/com/krisnaajiep/expensetrackerapi/security/config/package-info.java
@@ -2,8 +2,8 @@
  * This package contains configuration classes for security.
  * Configurations in this package include:
  * <ul>
- *   <li>Authentication configuration properties</li>
- *   <li>Security configurations</li>
+ *   <li>{@link com.krisnaajiep.expensetrackerapi.security.config.AuthProperties} to configure authentication properties</li>
+ *   <li>{@link com.krisnaajiep.expensetrackerapi.security.config.SecurityConfig} for security features setup</li>
  * </ul>
  *
  * @since 1.5.0
