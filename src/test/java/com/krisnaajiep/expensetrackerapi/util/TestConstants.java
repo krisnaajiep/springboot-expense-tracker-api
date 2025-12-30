5271481c769013d473f9473f021736dec92701e3
Krisna Ajie <krisnaajiep@gmail.com>
2025-12-31 04:29:29 +0700

    test: add TestConstants for database table and expense category names

---
+++ src/test/java/com/krisnaajiep/expensetrackerapi/util/TestConstants.java
@@ -0,0 +1,30 @@
+package com.krisnaajiep.expensetrackerapi.util;
+
+/*
+IntelliJ IDEA 2025.1 (Ultimate Edition)
+Build #IU-251.23774.435, built on April 14, 2025
+@Author krisna a.k.a. Krisna Ajie
+Java Developer
+Created on 30/06/25 02.15
+@Last Modified 30/06/25 02.15
+Version 1.0
+*/
+
+public final class TestConstants {
+    public static final String[] DATABASE_TABLE_NAMES = {
+            "Expense",
+            "ExpenseCategory",
+            "RefreshToken",
+            "UserAccount"
+    };
+
+    public static final String[] EXPENSE_CATEGORY_NAMES = {
+            "Others",
+            "Health",
+            "Clothing",
+            "Utilities",
+            "Electronics",
+            "Leisure",
+            "Groceries"
+    };
+}
